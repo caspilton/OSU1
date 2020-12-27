94b67a65f1c98c405fca577890ee966c91e61590
caspilton <76644856+caspilton@users.noreply.github.com>
2020-12-27 13:54:43 +0300

    Create Ex_2_variant_10.c

---
+++ Algorithms_and_Data_Structure/C/1_Expression_programming_and_linear_algorithms/Ex_2_variant_10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+double abs_f(double a) { // we need that, becouse abs(x) need int type 
+  if (a >= 0) return a;
+  return -a;
+}
+
+int main() {
+  double f, x, y, z;
+  printf("Domain of y(x): (-inf; + inf)\n");
+  scanf("%lf", &x);
+  scanf("%lf", &y);
+  scanf("%lf", &z);
+  f = (pow(y, 0.6) * 0.7 + abs_f(z) * 0.5) / ((cos(z) * 0.5 * 0.7) / (((0.4 - pow(x, 0.7) * 0.7) * 0.8)));
+  printf("%lf", f);
+}
