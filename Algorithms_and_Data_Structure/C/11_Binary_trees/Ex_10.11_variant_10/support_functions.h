14a01f3dfdd7adf81b331c880a29f14310813252
caspilton <76644856+caspilton@users.noreply.github.com>
2021-01-23 08:18:41 +0300

    Create support_functions.h

---
+++ Algorithms_and_Data_Structure/C/11_Binary_trees/Ex_10.11_variant_10/support_functions.h
@@ -0,0 +1,10 @@
+#ifndef SUPPORT_FUNCTIONS_H
+#define SUPPORT_FUNCTIONS_H
+
+#include <stdlib.h>
+#include "binary_tree.h"
+
+BinaryTree *btree_create_randomly(unsigned size);
+int btree_min(BinaryTree *tree);
+
+#endif
